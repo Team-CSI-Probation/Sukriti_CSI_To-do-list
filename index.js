8c14d0903f981a241b79ac20c0ad7eecf9f5106f
Sukriti Maurya <sukritimaurya.fzd@gmail.com>
2022-01-06 23:35:48 +0530

    delete

--- index.js
+++ index.js
@@ -8,9 +8,9 @@ function add(item,id,check,del){if(del){return "";}
     const checked = check ? check_icon : uncheck_icon;
     const striked= check ? strike : "";
     
-    const entry= `<li><i class="fa ${checked} " id= "${id}"></i>
+    const entry= `<li><i class="fa ${checked} " id= "${id}" role="done" ></i>
  <span class=" ${striked} "> ${item} </span>
- <i class="fa fa-trash" id= "${id}"></i></li>`;
+ <i class="fa fa-trash" id= "${id}" role="trash"></i></li>`;
 
     list.insertAdjacentHTML("beforeEnd", entry);
 
@@ -34,9 +34,26 @@ delete:false});
 
 let task_list=[];
 let id=0;
-
+//Completing the task
 function update(node)
 {node.classList.toggle(check_icon);
     node.classList.toggle(uncheck_icon);
 document.querySelector("span").classList.toggle(strike);
 task_list[node.id].check ? false : true;}
+
+
+//Deleting the task
+function trash(node)
+{node.parentNode.parentNode.removeChild(node.parentNode);
+task_list[node.id].delete= true;}
+
+list.addEventListener("click", function (event){
+let node=event.target;
+const role=node.attributes.role.value;
+if(role=="done")
+{update(node);}
+else
+{trash(node);}
+
+
+})
